2af008e164e44a58ab032212708ab77b694567d6
malin <malin.rosen@medieinstitutet.se>
2024-05-20 11:11:32 +0200

    creating components

---
+++ src/components/GamePresentation.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import PlayerList from './PlayerList.vue';
+
+</script>
+
+<template>
+<PlayerList/>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/Players.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { ref } from 'vue';
+import { Player } from './models/Player';
+import { log } from 'console';
+
+
+const PlayerNameX = "Spelare X :"
+const PlayerNameO = "Spelare O :"
+
+const playerInputX = ref("")
+const PlayerInputO = ref("")
+const error = ref("")
+
+
+const emit = defineEmits<{
+    (e: "addPlayer", text: string):void
+}>()
+
+const handleSubmit = () =>{
+    if(playerInputX.value && PlayerInputO.value !== ''){
+        emit('addPlayer', playerInputX.value)
+        emit('addPlayer', PlayerInputO.value)
+        playerInputX.value = "";
+        PlayerInputO.value = "";
+    }else{
+        error.value = "Fyll i båda fälten"
+    }
+}
+
+</script>
+
+<template>
+    <div class="playersForm">
+        <form @submit.prevent="handleSubmit">
+            <p>{{ PlayerNameX }}</p>
+            <input type="text" v-model="playerInputX">
+            <p>{{ PlayerNameO }}</p>
+            <input type="text" v-model="PlayerInputO">
+            <button>Spara</button>
+            <p> {{ error }}</p>
+        </form>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
